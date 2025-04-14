69c53f6b46bb734a1bb4fb2a57ad349ae3b13d59
ghembre1 <gbhembree22@amarillocollege.com>
2025-04-14 18:43:21 -0500

    Update project.js
    Added the email restriction for if the users email has @gmail in it.

--- Casita_Sedona_Template/project.js
+++ Casita_Sedona_Template/project.js
@@ -11,13 +11,15 @@ document.getElementById('myForm').addEventListener('submit', function (e) {
     try {
       let valid = true;
       //Gets users inputs from the form
-      const name = document.getElementById('myName').value;
-      const email = document.getElementById('myEmail').value;
-      const phone = document.getElementById('myPhone').value;
+      let name = document.getElementById('myName').value;
+      let email = document.getElementById('myEmail').value;
+      let phone = document.getElementById('myPhone').value;
+      //Restricts emails that end with @gmail.com
+      let restrictedDomainPattern = /@gmail\.com$/i;
       //This is the pattern the email needs to follow. I had to google how to get this pattern
-      const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
       //This is the pattern the phone needs to follow. I also had to google this pattern since it was already in my original website
-      const phonePattern = /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
+      let phonePattern = /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
       //If name is empty it will print'Name is required into the nameError space I provided up top
       if (name === '') {
         document.getElementById('nameError').textContent = 'Name is required.';
@@ -27,6 +29,11 @@ document.getElementById('myForm').addEventListener('submit', function (e) {
       if (!emailPattern.test(email)) {
         document.getElementById('emailError').textContent = 'Enter a valid email address.';
         valid = false; //If it is false it goes to the next if
+      }
+      //Test to see if email ends in @gmail.com and if it does it will print out the email error
+      else if (restrictedDomainPattern.test(email)) {
+        document.getElementById('emailError').textContent = 'Emails from gmail.com are not allowed.';
+        valid = false;
       }
       //If the phone the user input is not in the right format it will print 'Enter a valid phone address.' in the emailError space
       if (!phonePattern.test(phone)) {
